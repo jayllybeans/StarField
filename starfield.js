783e1e78f473cd97e787806392f5e97707b05f42
jayllybeans <jmvalidzic@email.neit.edu>
2025-10-30 10:30:54 -0400

    comments written

--- starfield.js
+++ starfield.js
@@ -2,7 +2,13 @@ let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d");
 
 gameLoop(){
+    //draw background
 
+    //draw stars
+
+    //the stars go shimmy
+
+    //recycle stars
 }
 
 setInterval(gameLoop, 50);
